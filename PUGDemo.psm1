871f74d4777bbf7769a3056e8c6e322684418370
Hurt <BHurt@columbia.com>
2017-08-16 16:08:52 -0700

    add functions

--- PUGDemo.psm1
+++ PUGDemo.psm1
@@ -1,3 +1,33 @@
-function Get-String {
-    write-host "Hello World!"
+
+function Test-FileHasKeyword {
+    param(
+        [string]$path
+    )
+
+    begin {}
+    process {
+        $lines = Get-Content -Path $path | Select-String 'Keyword'
+        if ($lines) {
+            Write-Output $true
+        }
+        else {
+            Write-Output $false
+        }
+    }
+    end {}
+}
+function Move-ContentFile {
+    param(
+        [parameter(ValueFromPipeline = $true)]
+        [string]$path
+    )
+    
+    begin {}
+    process {
+        if (Test-FileHasKeyword -path $path) {
+            Copy-Item -Path $path -Destination 'c:\windows\temp'
+        }
+    }
+    end {}
+
 }
